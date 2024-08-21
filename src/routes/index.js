3be16d88c3e735cac50d6b73062e7bac6fd8470e
Curry4182 <kangbk4182@naver.com>
2024-08-21 16:37:15 +0900

    refactor: 라우터 path를 저장하는 js파일 위치 변경

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import AskView from '@/views/AskView.vue'
+import ItemView from '@/views/ItemView.vue'
 import JobsView from '@/views/JobsView.vue'
 import NewsView from '@/views/NewsView.vue'
+import UserView from '@/views/UserView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -24,6 +26,14 @@ const router = createRouter({
             path: '/jobs',
             name: 'JOBS',
             component: JobsView
+        },
+        {
+            path: '/user',
+            component: UserView
+        },
+        {
+            path: '/item',
+            component: ItemView
         }
     ]
 })
